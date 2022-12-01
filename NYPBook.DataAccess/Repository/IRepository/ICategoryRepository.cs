081fb20dc86002159c75314763996891ad6898d7
Nirmeet-DEV1204 <dev.nirmeet07mail.com>
2022-12-01 14:15:59 -0500

    added Repository And I repositroy with IUnitOfWork and UnitOfWork

---
+++ NYPBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using NYPBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NYPBook.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
