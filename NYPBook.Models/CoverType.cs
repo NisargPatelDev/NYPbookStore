cbc46f61fd50719773e93fb9b2ac888c993843e0
Nisarg Patel <nisargpateldev@gmail.com>
2022-12-05 12:47:06 -0500

    Add files via upload

--- NYPBook.Models/CoverType.cs
+++ NYPBook.Models/CoverType.cs
@@ -5,12 +5,12 @@ using System.Text;
 
 namespace NYPBook.Models
 {
-    public class Category
+    public class CoverType
     {
         [Key]
         public int Id { get; set; }
-        [Display(Name = "Category Name")]
         [Required]
+        [Display(Name = "Cover Type")]
         [MaxLength(50)]
         public string Name { get; set; }
     }
